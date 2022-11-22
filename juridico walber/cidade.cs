2b6705320eca7e4e5199ab7281f40db3c0e26b7c
walbernetolima2022 <118470432+walbernetolima2022@users.noreply.github.com>
2022-11-22 10:06:09 -0300

    Add files via upload

---
+++ juridico walber/cidade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace judiciario
+{
+    public class cidade
+    {
+        public int codigo { get; set; }
+        public string descricao { get; set; }
+        public string Estado { get; set; }
+
+        public List<cidade> getTodascidades()
+        {
+            return n11;
+        }
+
+        public List<Cidade> getRetornaCidade(int idCidade)
+        {
+            return nu11;
+        }
+
+        public List<cidade> getCidadePorEstado()
+        {
+            return n11;
+        }
+
+    }
+}
---
+++ juridico walber/estado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace judiciario
+{
+    public class estado
+    {
+        public int codigo { get; set; }
+        public string descricao  { get; set; }
+        public string sigle { get; set; }
+
+        public List<estado> getTodosEstados()
+        {
+            return n11;
+        }
+        public List<estado> getRetornaEstado(int idEstado)
+        {
+            return n11;
+        }
+    }
+}
+}
